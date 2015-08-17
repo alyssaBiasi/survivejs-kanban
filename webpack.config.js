d95474e94f345b8c1c7b72ec0902c54271d37a16
Alyssa Biasi <alyssa.biasi@rea-group.com>
2015-08-17 21:27:08 +1000

    Webpack configuration 'start' target

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 var path = require('path');
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 
+var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
 
-module.exports = {
+var common = {
   entry: path.resolve(ROOT_PATH, 'app/main'),
   output: {
     path: path.resolve(ROOT_PATH, 'public'),
@@ -24,4 +26,20 @@ module.exports = {
     new HtmlwebpackPlugin({ title: 'Kanban App' })
   ]
 };
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      colors: true,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
 
