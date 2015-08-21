4e13c2be5441402a8b000a88bd748a02bfeeff3d
Alyssa Biasi <biasi91@gmail.com>
2015-08-21 11:35:00 +1000

    Make Note components editable
    Changes thrown away at the moment

--- src/js/components/Note.jsx
+++ src/js/components/Note.jsx
@@ -1,8 +1,57 @@
 import React from 'react';
 
 class Note extends React.Component {
+  constructor(props) {
+    super(props);
+    this.edit = this.edit.bind(this);
+    this.renderTask = this.renderTask.bind(this);
+    this.renderEdit = this.renderEdit.bind(this);
+    this.checkEnter = this.checkEnter.bind(this);
+    this.finishEdit = this.finishEdit.bind(this);
+
+    this.state = {
+      editing: false
+    };
+  }
+
   render() {
-    return <div>{ this.props.task }</div>;
+    const editing = this.state.editing;
+
+    return (
+      <div>
+        { editing ? this.renderEdit() : this.renderTask() }
+      </div>
+    );
+  }
+
+  renderEdit() {
+    return <input type='text'
+                  autoFocus={true}
+                  defaultValue={this.props.task}
+                  onKeyPress={this.checkEnter}
+                  onBlur={this.finishEdit} />;
+  }
+
+  renderTask() {
+    return <div onClick={this.edit}>{ this.props.task }</div>;
+  }
+
+  edit() {
+    this.setState({
+      editing: true
+    });
+  }
+
+  checkEnter(e) {
+    if(e.key == 'Enter') {
+      this.finishEdit(e);
+    }
+  }
+
+  finishEdit(e) {
+    this.setState({
+      editing: false
+    });
   }
 }
 
