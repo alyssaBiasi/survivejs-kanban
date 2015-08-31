0f5fc7b7ed6f1423f1bee798f4179cfb7b4c6187
Alyssa Biasi <biasi91@gmail.com>
2015-08-31 19:28:20 +1000

    Swap decorator for AltContainer

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import AltContainer from 'alt/AltContainer';
 import Notes from './Notes.jsx';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
-import connect from '../decorators/connect';
 
-@connect(NoteStore)
 export default class App extends React.Component {
-
   render() {
-    const items = this.props.notes || [];
+    const items = () => NoteStore.getState().notes;
 
     return (
       <div>
         <h1>To Do:</h1>
-        <Notes items={items} onEdit={this.editNote} onDelete={this.deleteNote} />
         <button onClick={this.addNote}> + </button>
+        <AltContainer stores={[NoteStore]}
+                      inject={{items: items}} >
+          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+        </AltContainer>
       </div>
     );
   }
